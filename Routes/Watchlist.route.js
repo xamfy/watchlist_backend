20165b37128be10e37eb992212b0cdd3ed6cd37e
xamfy <spratyaksh22@gmail.com>
2020-11-10 23:00:40 +0530

    Allow only unique Movie object IDs in Watchlist collection Fixes #18

--- Routes/Watchlist.route.js
+++ Routes/Watchlist.route.js
@@ -22,14 +22,45 @@ router.route("/add").post((req, res) => {
   const name = req.body.name;
   const watchlistId = req.body.watchlistId;
   const movieId = req.body.movieId;
+  console.log(movieId);
 
-  Watchlist.findByIdAndUpdate(
-    watchlistId,
-    { $push: { movies: movieId } },
-    { new: true, useFindAndModify: false }
-  )
-    .then(() => res.json("Movie added to watchlist"))
+  Watchlist.findById(watchlistId)
+    .then((watchlist) => {
+      // res.json(watchlist);
+      if (watchlist.movies.includes(movieId)) {
+        res.send("already exists");
+      } else {
+        // res.send("doesn't exist");
+        Watchlist.findByIdAndUpdate(
+          watchlistId,
+          { $push: { movies: movieId } },
+          { new: true, useFindAndModify: false }
+        )
+          .then(() => res.json("Movie added to watchlist"))
+          .catch((err) => res.status(400).json("Error: " + err));
+      }
+    })
     .catch((err) => res.status(400).json("Error: " + err));
+
+  // Watchlist.find({ movies: { $in: [movieId] } }, (err, movie) => {
+  //   if (err) {
+  //     // console.log(err)
+  //     res.send(err);
+  //   }
+  //   console.log(movie);
+  //   if (movie.length > 0) {
+  //     res.status(400).json("Movie already exists in the watchlist");
+  //     return;
+  //   } else {
+  //     Watchlist.findByIdAndUpdate(
+  //       watchlistId,
+  //       { $push: { movies: movieId } },
+  //       { new: true, useFindAndModify: false }
+  //     )
+  //       .then(() => res.json("Movie added to watchlist"))
+  //       .catch((err) => res.status(400).json("Error: " + err));
+  //   }
+  // });
 });
 
 module.exports = router;
